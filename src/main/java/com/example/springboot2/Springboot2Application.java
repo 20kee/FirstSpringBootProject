03affea5d678b57adcf38b33b4eeda60c176f785
20kee <20kee@naver.com>
2023-12-23 17:57:02 +0900

    until internationalized

--- src/main/java/com/example/springboot2/Springboot2Application.java
+++ src/main/java/com/example/springboot2/Springboot2Application.java
@@ -1,9 +1,12 @@
 package com.example.springboot2;
 
-import org.hibernate.service.spi.Configurable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class Springboot2Application {
@@ -11,6 +14,13 @@ public class Springboot2Application {
     public static void main(String[] args) {
 
         ApplicationContext ac = SpringApplication.run(Springboot2Application.class, args);
+    }
+
+    @Bean
+    public SessionLocaleResolver localResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.US);
+        return localeResolver;
     }
 
 }
